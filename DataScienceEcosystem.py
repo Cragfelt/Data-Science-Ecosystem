ebcd7a184ec795d2ab5c919f3e54a867c5d1b532
Diego Aguilar <30749984+Cragfelt@users.noreply.github.com>
2023-08-22 15:10:04 -0600

    Add files via upload
    Jupyter Notebook created online. Exercise 12, week 5.

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+# 
+# 
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives:**
+# 
+# 1. Categories of Data Science Tools
+# 2. Open Source Tools for Data Science
+# 3. Commercial and Clud Based Tools for Data Science
+# 4. Application Programming Interfaces (APIs)
+# 5. Machine Learning Models
+# 6. R Basics with RStudio and GitHub Repositories
+# 
+# * Popular languages for Data Scientists
+# * Common libraries used by Data Scientists
+# * Data Science Tools
+# * Examples of arithmetic expressions evaluation in Python
+# * Sharing notebooks through GitHub
+
+# Some of the popular languages that Data Scientists use are:
+#     
+#     1. R
+#     2. Python
+#     3. SQL
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+#     1. NumPy
+#     2. Pandas
+#     3. ggplot
+#     4. TensorFlow
+#     5. Matplotlib
+
+# Data Science Tools
+# 
+# | Column 1   |
+# |:----------:|
+# |  RStudio |
+# |  Apache Spark   |
+# |  TensorFlow   |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+# 
+# 
+
+# In[2]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+
+x = (3 * 4) + 5
+print(x)
+
+
+# In[8]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+
+min = int(input("Enter time in minutes: "))
+h = min / 60
+print("Hours=",h)
+
+
+# ## Author
+# 
+# Luis Diego Aguilar
